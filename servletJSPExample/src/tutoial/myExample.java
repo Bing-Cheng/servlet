8b8d42adf3ea80b1dc1e5f11feb3d791b21dcb93
Aimee <Aimee@172.31.17.33>
2013-04-21 21:40:23 -0700

    initial

---
+++ servletJSPExample/src/tutoial/myExample.java
@@ -0,0 +1,57 @@
+package tutoial;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class myExample
+ */
+@WebServlet("/myExample")
+public class myExample extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public myExample() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//PrintWriter out  = response.getWriter();
+		if(request.getParameter("user")==null)
+		{
+		getServletContext().getRequestDispatcher("/index.jsp").forward(request,response);
+		return;
+		}
+		String userName = request.getParameter("user");
+		//out.println("hello world" + userName);
+		request.setAttribute("user",userName);
+		getServletContext().getRequestDispatcher("/output.jsp").forward(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
