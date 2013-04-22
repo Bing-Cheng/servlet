6fa739fe6256175389b1711da3a06a5cb454a820
Aimee <Aimee@172.31.17.33>
2013-04-21 22:05:59 -0700

    second

--- servletJSPExample/src/tutoial/myExample.java
+++ servletJSPExample/src/tutoial/myExample.java
@@ -28,16 +28,16 @@ public class myExample extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//PrintWriter out  = response.getWriter();
+		PrintWriter out  = response.getWriter();
 		if(request.getParameter("user")==null)
 		{
 		getServletContext().getRequestDispatcher("/index.jsp").forward(request,response);
-		return;
+		//return;
 		}
 		String userName = request.getParameter("user");
-		//out.println("hello world" + userName);
-		request.setAttribute("user",userName);
-		getServletContext().getRequestDispatcher("/output.jsp").forward(request,response);
+		out.println("hello world" + userName);
+		//request.setAttribute("user",userName);
+		//getServletContext().getRequestDispatcher("/output.jsp").forward(request,response);
 	}
 
 	/**
